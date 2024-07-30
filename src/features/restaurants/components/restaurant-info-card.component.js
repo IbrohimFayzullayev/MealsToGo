945e2cf916e6954ef823ace28a0897e98166dd1e
Ibrohim Fayzullayev <ibrohim_f01@mail.ru>
2024-07-30 18:30:08 +0500

    add

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+    address = "100 some render street",
+  } = restaurant;
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: "white",
+  },
+  title: {
+    padding: 16,
+  },
+});
