35e1ec970a53d4ca2cbb602ea86d9fac60cbff4c
Ibrohim Fayzullayev <ibrohim_f01@mail.ru>
2024-08-07 17:35:54 +0500

    add

---
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
+
+const RestaurantStack = createStackNavigator();
+
+export const RestaurantsNavigator = () => {
+  return (
+    <RestaurantStack.Navigator
+      screenOptions={{
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
+    >
+      <RestaurantStack.Screen
+        options={{ headerShown: false }}
+        name="Restaurant"
+        component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
+      />
+    </RestaurantStack.Navigator>
+  );
+};
