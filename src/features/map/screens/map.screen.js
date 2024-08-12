001d99653b26c0fa8674ed85ecf69ce764dd1b22
IbrohimFayzullayev <ibrohim_f01@mail.ru>
2024-08-12 11:07:38 +0500

    add

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import MapView from "react-native-maps";
+import MapView, { Callout } from "react-native-maps";
 import styled from "styled-components";
 
 import { Search } from "../component/search.component";
 import { LocationContext } from "../../../services/location/location.context";
 import { Marker } from "react-native-maps";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../component/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -47,7 +48,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <Callout>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
           );
         })}
       </Map>
