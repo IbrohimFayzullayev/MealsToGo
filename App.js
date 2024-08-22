f1a909c743f1d5787d33438736801daf6612fcfc
Ibrohim Fayzullayev <ibrohim_f01@mail.ru>
2024-08-22 17:21:31 +0500

    change auth

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 import {
   useFonts as useOswald,
@@ -9,9 +10,22 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
+import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
 
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNwfWW108GnG19xWjNzc8BliU2RQki610",
+  authDomain: "mealstogo-a4028.firebaseapp.com",
+  projectId: "mealstogo-a4028",
+  storageBucket: "mealstogo-a4028.appspot.com",
+  messagingSenderId: "314069792316",
+  appId: "1:314069792316:web:d6d67b2dba1cb93093b539",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,22 +1,8 @@
-import { initializeApp } from "firebase/app";
 import {
   createUserWithEmailAndPassword,
-  getAuth,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCNwfWW108GnG19xWjNzc8BliU2RQki610",
-  authDomain: "mealstogo-a4028.firebaseapp.com",
-  projectId: "mealstogo-a4028",
-  storageBucket: "mealstogo-a4028.appspot.com",
-  messagingSenderId: "314069792316",
-  appId: "1:314069792316:web:d6d67b2dba1cb93093b539",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+import { auth } from "../../../App";
 
 export const loginRequest = async (email, password) =>
   signInWithEmailAndPassword(auth, email, password);
